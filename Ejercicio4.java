03f6042e7bf686b6179307ca7c56583d7df662fe
AraSCervi <asancer837@g.educaand.es>
2022-10-23 21:16:41 +0200

    Añadido archivo modificado Ejercicio3.java

---
+++ Ejercicio4.java
@@ -0,0 +1,18 @@
+/**
+* Ejercicio 4. Muestra un horario clase
+* 
+* @asc
+*/
+public class Ejercicio4 {  // clase principal
+    public static void main(String[] args)  {
+      System.out.println(" Lunes\tMartes\tMierc.\tJueves\tViernes");
+      System.out.println("=======================================");
+      System.out.println("Guardia\tProgDAM\t\tProgDAM\tProgDAM");
+      System.out.println("ProgDAM\tProgDAM\tElabMat\tProgDAM\tProgDAM");
+      System.out.println("ProgDAM\tProgDAW\tRef.\tProgDAW\tProgDAW");
+      System.out.println("ElabMat\tProgDAW\tGuardia\tProgDAW\tProgDAW");
+      System.out.println("ElabMat\tRef\t\tProgDAW");
+      System.out.println("Guardia\t\t\tProgDAW\t");
+    
+  }
+}
